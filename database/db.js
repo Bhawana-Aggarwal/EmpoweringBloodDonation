4d4355478c4d883ea7c7c9f7a951002f09ac56e0
Bhawana <bhawanaagarwal7988@gmail.com>
2025-02-02 11:40:17 +0530

    Stopped tracking .env

--- database/db.js
+++ database/db.js
@@ -6,7 +6,9 @@ require('dotenv').config();
 const mongoURL = process.env.MONGO_URL;
 
 mongose.connect(mongoURL,{
-
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    tls: true,
 });
     
 const db = mongose.connection;
