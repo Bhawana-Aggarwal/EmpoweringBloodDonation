f5b1d02f68e0fd655ba92582428fbad254d3b943
Bhawana <bhawanaagarwal7988@gmail.com>
2025-03-04 20:21:23 +0530

    Remove animation

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use('/admin', adminRoutes);
 
 
 // port listening on 'port'
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000 ;
 app.listen(port, () => {
     console.log(`Server running on the port : ${port}`);
 })
